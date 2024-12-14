eae113d860f2c34722b3c369e9e8bd5f7fd42e42
corderaa <ugaitzcordero1@gmail.com>
2024-12-15 00:42:26 +0100

    added laravel, npm, vite partial support

--- index.js
+++ index.js
@@ -9,30 +9,34 @@ import inquirer from 'inquirer';
 import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 import figlet from 'figlet';
+import fs from 'fs';
 import { createSpinner } from 'nanospinner';
 import { execSync } from 'child_process';
 import { Str } from '@supercharge/strings'
     
 
 let directory;
+let splitedOutput;
+let formatedOutput;
 
-const output = execSync('find ~ -name ".git" -type d', { encoding: 'utf-8' });  // the default is 'buffer'
-var splitedOutput = Str(output).lines(); 
+
+const COMPOSER_FILE_NAME = 'composer.json'
+const NODE_FILE_NAME = 'package.json'
+const VITE_FILE_NAME = 'vite.config.js'
+
 //console.log(chalk.bgBlackBright(splitedOutput[0]));
 
-const sleep = (ms = 4000) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
 async function welcome(){
-    const title = chalkAnimation.neon('REPOVIEW-CLI');
-
-
+    const title = chalkAnimation.glitch('REPOVIEWER-CLI');
     await sleep();
     title.stop();
 
     console.log(`
 ${chalk.bgCyan('HOW TO USE')}
-Select a repo from the list bellow
-REPOVIWER will automatically open your enviroment  
+Select a repo from the list below
+REPOVIEWER will automatically open your enviroment  
 `)
 }
 
@@ -40,7 +44,7 @@ async function askDirectory(){
     const answers = await inquirer.prompt({
         name: 'directory',
         type: 'input',
-        message: 'The directory that cointains your repos: [LEAVE BLANK IF "HOME"]',
+        message: 'The directory that cointains your repos: [LEAVE IT BLANK IF "home"]',
         default(){
             return directory;
         }
@@ -50,20 +54,57 @@ async function askDirectory(){
 }
 
 async function repoList(){
+
+    const output = execSync('find '+ directory +' -name ".git" -type d', { encoding: 'utf-8' });  // the default is 'buffer'
+    splitedOutput = Str(output).lines();
+
+    formatedOutput = splitedOutput.map(line => line.replace('.git', ''));
+
     const answers = await inquirer.prompt({
         name:'repo',
         type: 'list',
         message: 'Choose your repo',
-        choices: splitedOutput,
-
+        choices: formatedOutput,
     });
 
     return openRepo(answers.repo);
 }
 
-async function openRepo(){
-    const spinner = createSpinner('Openning repo.....').start();
+async function openRepo(repo){
+
+
+    const composerPath = repo + '/' + COMPOSER_FILE_NAME;
+    const nodePath = repo + '/' + NODE_FILE_NAME;
+    const vitePath = repo + '/' + VITE_FILE_NAME;
+
+    const spinner = createSpinner('Openning repo..... \n').start();
     await sleep();
+
+    if(fs.existsSync(composerPath)){
+        await execSync('cd '+ repo +'&& ./vendor/bin/sail up -d',{ stdio: 'inherit' });
+        console.log(chalk.bold.black.bgWhite('laravel environment started'));
+
+    }
+    
+    if(fs.existsSync(nodePath)){
+        try {
+            await execSync('cd '+ repo +' && sudo npm install',{ stdio: 'inherit' });
+            console.log(chalk.white.bgBlack('node modules installed / updated'));
+        } catch (error) {
+            console.log(chalk.red.bold.bgBlackBright('Error installing node modules'));
+        }
+    }
+
+    if(fs.existsSync(vitePath)){
+        try {
+            await execSync('cd '+ repo +' && sudo npm run build',{ stdio: 'inherit' });
+            console.log(chalk.white.bgBlack('vite started'));
+        } catch (error2) {
+            console.log(chalk.red.bold.bgBlackBright('Error starting vite:'));
+        }
+    }
+
+    spinner.stop();
 }
 
 await welcome()
